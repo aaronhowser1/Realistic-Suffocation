b3d32f18c7fa4d799245b7cfe6124b6a8ece3900
Aaron Howser <aaronhowser1@gmail.com>
2024-04-29 05:44:13 -0500

    Scooped out the parts i dont need, and moved from examplemod etc

---
+++ src/main/java/com/aaronhowser/mods/realisticsuffocation/RealisticSuffocation.java
@@ -0,0 +1,45 @@
+package com.aaronhowser.mods.realisticsuffocation;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.client.Minecraft;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import org.slf4j.Logger;
+
+@Mod(RealisticSuffocation.MODID)
+public class RealisticSuffocation {
+    public static final String MODID = "realisticsuffocation";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public RealisticSuffocation(IEventBus modEventBus, ModContainer modContainer) {
+        modEventBus.addListener(this::commonSetup);
+
+        NeoForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {
+        LOGGER.info("HELLO FROM COMMON SETUP");
+    }
+
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {
+        LOGGER.info("HELLO from server starting");
+    }
+
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
+        }
+    }
+}
